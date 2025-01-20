f36abb43a9c993b602717bf079706d2c98f3d712
Danny Landa <Danny.Landa@jdplc.com>
2025-01-20 11:41:26 +0000

    updated the wordpress install script to include S3 peristant storage

--- wordpress-installation-setup.sh
+++ wordpress-installation-setup.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 sudo rm -rf /var/www/html
+snap install aws-cli --classic
 sudo apt -y install unzip
 sudo wget -O /var/www/latest.zip https://wordpress.org/latest.zip
 sudo unzip /var/www/latest.zip -d /var/www/
 sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html 
 
-password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
 username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
 
-echo $password > creds.txt
 echo $username >> creds.txt
-sudo mv creds.txt /root/ai-content-application/
+echo $password > creds.txt
+sudo mv creds.txt /root/EPA-Project
+
+# Retrieve credentials (adjust the file path if needed)
+username=$(tail -n 1 /root/EPA-Project/creds.txt)
+password=$(head -n 1 /root/EPA-Project/creds.txt)
+
+aws s3 cp s3://brandscribe-backup/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
+sudo gunzip /tmp/wordpress_dump.sql.gz
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+sudo mysql $username < /tmp/wordpress_dump.sql
+sudo rm /tmp/wordpress_dump.sql
 
 # sudo mariadb -u root
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
-sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost identified by '$password'"
-sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
-sudo mysql -e "FLUSH PRIVILEGES"
+# sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+# sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost identified by '$password'"
+# sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
+# sudo mysql -e "FLUSH PRIVILEGES"
 
 # sudo wget -O /var/www/html/wp-config.php https://dannylandawordpress.s3.amazonaws.com/wp-config.php
 
