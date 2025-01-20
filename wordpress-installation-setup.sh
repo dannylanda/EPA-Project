95d1193abe2ac092c1549e61fd686a8dce1191a7
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2025-01-20 16:10:34 +0000

    Update wordpress-installation-setup.sh
    new script with -y and using old database credentials

--- wordpress-installation-setup.sh
+++ wordpress-installation-setup.sh
@@ -1,39 +1,3 @@
-# #!/bin/bash 
-
-# # Entering the html directory 
-# cd /var/www/html
-
-# # Installing the unzip package
-# sudo apt -y install unzip 
-
-# # Install/Unzip/Remove WordPress
-# sudo wget https://wordpress.org/latest.zip 
-# sudo unzip latest.zip  
-# sudo rm latest.zip 
-
-# # Generate password for use in WordPress Database
-# username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
-# password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
-
-# echo $username >> creds.txt
-# echo $password > creds.txt
-
-# # Create a MariaDB Database and a User for the WordPress Site  
-# sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
-# sudo mysql -e "CREATE USER $username@localhost identified by '$password'"
-# sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* to $username@localhost"
-# sudo mysql -e "FLUSH PRIVILEGES" # Applies everything you've done 
-
-# sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-# sudo chmod 640 /var/www/html/wp-config.php 
-# sudo chown -R www-data:www-data /var/www/html/wordpress
-
-# sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
-# sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
-# sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
-
-############################################################################
-
 #!/bin/bash 
 
 # Entering the html directory 
@@ -47,31 +11,34 @@ sudo apt -y install unzip
 
 # Install/Unzip/Remove WordPress
 sudo wget https://wordpress.org/latest.zip 
-sudo unzip latest.zip  
-sudo rm latest.zip 
+sudo unzip -o latest.zip  # Overwrite any existing files without prompting
+sudo rm -f latest.zip     # Force removal without confirmation
 
-# Generate password for use in WordPress Database
-username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
-password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
+# Download the credentials file from S3
+aws s3 cp "s3://brandscribe-backup/creds for database dump.txt" /tmp/creds.txt
 
-echo $username > root/EPA-Project/creds.txt
-echo $password >> root/EPA-Project/creds.txt
+# Extract username and password from the credentials file
+username=$(grep -i "username" /tmp/creds.txt | awk '{print $2}')
+password=$(grep -i "password" /tmp/creds.txt | awk '{print $2}')
 
-# Connect to S3 Bucket
-aws s3 cp s3://brandscribe-backup/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
-sudo gunzip /tmp/wordpress_dump.sql.gz
-password=$(head -n 1 /root/EPA-Project/creds.txt)
-username=$(tail -n 1 /root/EPA-Project/creds.txt)
+# Create the MariaDB Database and User using extracted credentials
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+sudo mysql -e "CREATE USER $username@localhost IDENTIFIED BY '$password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@localhost"
+sudo mysql -e "FLUSH PRIVILEGES"
+
+# Connect to S3 Bucket and restore the WordPress database dump
+aws s3 cp s3://brandscribe-backup/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
+sudo gunzip -f /tmp/wordpress_dump.sql.gz  # Force overwrite if the file already exists
 sudo mysql $username < /tmp/wordpress_dump.sql
-sudo rm /tmp/wordpress_dump.sql
+sudo rm -f /tmp/wordpress_dump.sql  # Force removal without confirmation
 
 # Set up the WordPress config file
-sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+sudo mv -f /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php  # Force overwrite
 sudo chmod 640 /var/www/html/wordpress/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html/wordpress
 
 # Update wp-config.php with the database credentials
-sed -i "s/password_here/$password/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/$username/g" /var/www/html/wordpress/wp-config.php
 sed -i "s/database_name_here/$username/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$password/g" /var/www/html/wordpress/wp-config.php
