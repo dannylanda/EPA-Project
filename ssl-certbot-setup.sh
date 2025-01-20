c250c4f18ef5aed8becc7376e99f32752725e771
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2025-01-20 12:55:20 +0000

    Update ssl-certbot-setup.sh
    updated path due to script failure

--- ssl-certbot-setup.sh
+++ ssl-certbot-setup.sh
@@ -23,4 +23,4 @@ systemctl reload nginx.service
 sudo nginx -t && sudo systemctl reload nginx
 
 # Run the WordPress installation script
-sudo bash /root/EPA-Project/wordpress-install.sh
+sudo bash /root/EPA-Project/wordpress-install-setup.sh
