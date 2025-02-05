fcc6f2598036a5a4297a981b864dfa5be1337593
Danny Landa <danlanda@hotmail.com>
2025-02-05 16:35:35 +0000

    final cleanup of the folder, changed iam name to match region and updated bash scripts now to incorperate s3 cp now that files are in bucket

--- bash_scripts/backend.sh
+++ bash_scripts/backend.sh
@@ -16,19 +16,19 @@ sudo systemctl restart mariadb
 username=DB_USERNAME
 password=DB_PASSWORD
 
-echo $username > creds.txt
-echo $password >> creds.txt
+echo $username > /home/ubuntu/EPA-Project/creds.txt
+echo $password >> /home/ubuntu/EPA-Project/creds.txt
 
 # Connect to S3 Bucket
-#sudo aws s3 cp s3://brandscribe-backup/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
+sudo aws s3 cp s3://brandscribe-backup/wordpress_dump.sql /tmp/wordpress_dump.sql
 #sudo gunzip /tmp/wordpress_dump.sql.gz
 
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 sudo mysql -e "CREATE USER IF NOT EXISTS '$username'@'FRONTEND_IP' IDENTIFIED BY '$password'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO '$username'@'FRONTEND_IP'"
 sudo mysql -e "FLUSH PRIVILEGES"
-# sudo mysql $username < /tmp/wordpress_dump.sql
-# sudo rm /tmp/wordpress_dump.sql
+sudo mysql $username < /tmp/wordpress_dump.sql
+sudo rm /tmp/wordpress_dump.sql
 
 # This securely stores the credentials file in AWS S3 for later use or backup
 sudo aws s3 cp /home/ubuntu/EPA-Project/creds.txt s3://brandscribe-backup
