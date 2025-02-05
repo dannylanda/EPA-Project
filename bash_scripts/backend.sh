6961618aeccd0c0f4bed2d198b10bebe5cb3358f
Danny Landa <79216206+dannylanda@users.noreply.github.com>
2025-02-05 10:21:19 +0000

    Update backend.sh
    commenting out S3 command copying over the dump while i export a new dump outwards to pull in later

--- bash_scripts/backend.sh
+++ bash_scripts/backend.sh
@@ -20,14 +20,15 @@ echo $username > creds.txt
 echo $password >> creds.txt
 
 # Connect to S3 Bucket
-sudo aws s3 cp s3://brandscribe-backup/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
-sudo gunzip /tmp/wordpress_dump.sql.gz
+#sudo aws s3 cp s3://brandscribe-backup/wordpress_dump.sql.gz /tmp/wordpress_dump.sql.gz
+#sudo gunzip /tmp/wordpress_dump.sql.gz
+
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 sudo mysql -e "CREATE USER IF NOT EXISTS '$username'@'FRONTEND_IP' IDENTIFIED BY '$password'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO '$username'@'FRONTEND_IP'"
 sudo mysql -e "FLUSH PRIVILEGES"
-sudo mysql $username < /tmp/wordpress_dump.sql
-sudo rm /tmp/wordpress_dump.sql
+# sudo mysql $username < /tmp/wordpress_dump.sql
+# sudo rm /tmp/wordpress_dump.sql
 
 # This securely stores the credentials file in AWS S3 for later use or backup
-sudo aws s3 cp creds.txt s3://brandscribe-backup
+# sudo aws s3 cp creds.txt s3://brandscribe-backup
